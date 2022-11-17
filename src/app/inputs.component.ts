c9afdf56b8b7948fa1f7a4a21415421c72cc1561
Anastasiya.Golova <Anastasiya.Golova@waveaccess.ru>
2022-11-17 20:49:35 +0400

    fix

--- src/app/inputs.component.ts
+++ src/app/inputs.component.ts
@@ -1,5 +1,5 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {CalcData} from "./app.component";
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CalcData } from "./app.component";
 
 @Component({
   selector: 'inputs',
@@ -12,6 +12,7 @@ export class InputsComponent {
 
   inputNum1: string = '1';
   inputNum2: string = '1';
+  pattern: RegExp = /[]/
 
   operations = [
     "Сложить",
@@ -21,13 +22,37 @@ export class InputsComponent {
   ];
   inputSelectedValue = (this.operations)[0];
 
-  systems = [
-    "2",
-    "8",
-    "10",
-    "16"
-  ];
-  inputSelectedValueSystem = (this.systems)[0];
+  System: any = ['2', '8', '10', '16'];
+  param3_val: any
+
+  changeSystem(e: any): string {
+    console.log(e.target.value)
+    this.param3_val = e.target.value
+    return this.param3_val
+  }
+
+  keyPress(event: KeyboardEvent, pattern: any) {
+    const inputChar = String.fromCharCode(event.charCode);
+    if (!pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+
+  getPatternParam() {
+    if (this.param3_val == '2') {
+      this.pattern = /[0-1]/
+    }
+    if (this.param3_val == '8') {
+      this.pattern = /[0-7]/
+    }
+    if (this.param3_val == '16') {
+      this.pattern = /[0-9A-F]/
+    }
+    if (this.param3_val == '10') {
+      this.pattern = /[0-9]/
+    }
+    return this.pattern
+  }
 
   emitCalcData() {
     this.inputNum2 = this.inputNum2;
@@ -35,7 +60,7 @@ export class InputsComponent {
       num1: this.inputNum1,
       num2: this.inputNum2,
       selectedValue: this.inputSelectedValue,
-      system: this.inputSelectedValueSystem
+      system: this.param3_val
     };
 
     this.calcDataEvent.emit(calcData);
